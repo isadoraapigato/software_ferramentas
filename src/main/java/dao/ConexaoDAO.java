34d78bda4da40ff79b5cd225efb7959f2f35386a
Isadora Velho <116107592+isadoraapigato@users.noreply.github.com>
2024-05-19 19:10:05 -0300

    feat: Conexão com banco de dados

--- src/main/java/dao/ConexaoDAO.java
+++ src/main/java/dao/ConexaoDAO.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-/**
- *
- * @author isado
- */
 public class ConexaoDAO {
-    
+    public Connection getConexao() {
+        Connection connection = null; // instância da conexão
+        try {
+            // Carregamento do JDBC Driver
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
+            // Configurar a conexão
+            String server = "localhost";
+            String database = "emprestimo";
+            String url = "jdbc:mysql://" + server + ":3306/"
+            +database + "?useTimezone=true&serverTimezone=UTC";
+            String user = "root";
+            String password = "admin123";
+            // Conectando..
+            connection = DriverManager.getConnection(url, user, password);
+            // Testando..
+            if (connection != null) {
+                System.out.println("Status: Conectado!");
+            } else {
+                System.out.println("Status: NÃO CONECTADO!");
+            }
+            return connection;
+        } catch (ClassNotFoundException e) { // Driver não encontrado
+            System.out.println("O driver não foi encontrado.");
+            return null;
+        } catch (SQLException e) {
+            System.out.println("Não foi possível conectar...");
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
