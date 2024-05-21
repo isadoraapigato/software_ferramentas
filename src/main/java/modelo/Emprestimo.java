8afaa81cecb2a785c8d48b11ea60a003711a51d9
annajusantos <annajuliasantoos@gmail.com>
2024-05-21 20:31:49 -0300

    feat: Implementação do modelo empréstimo

--- src/main/java/modelo/Emprestimo.java
+++ src/main/java/modelo/Emprestimo.java
@@ -1,4 +1,38 @@
 package modelo;
+
 public class Emprestimo {
+    private String DataDevolucao;
+    private String DataEmprestimo;
+            
+    public Emprestimo () {
+        this("","");
+               
+    }
+
+    public Emprestimo(String DataDevolucao, String DataEmprestimo) {
+        this.DataDevolucao = DataDevolucao;
+        this.DataDevolucao = DataEmprestimo;
+    }
     
+    public String getDataDevolucao() {
+        return DataDevolucao;
+    }
+    
+    public String getDataEmprestimo() {
+        return DataEmprestimo;
+    }
+
+    public void setDataDevolucao(String DataDevolucao) {
+         this.DataDevolucao = DataDevolucao;
+    }
+
+    public void setDataEmprestimo(String DataEmprestimo) {
+         this.DataEmprestimo = DataEmprestimo;
+    }
+
 }
+
+
+        
+    
+
